b1f462ffd8491425b1c114da08706c1e0788f572
soulomoon <fwy996602672@gmail.com>
2021-12-22 12:46:21 +0000

    update

--- home/home.nix
+++ home/home.nix
@@ -28,7 +28,6 @@
     # btop
   ];
   # programs.tmux.enable = true;
-  # home.stateVersion = "22.05";
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.git = {
