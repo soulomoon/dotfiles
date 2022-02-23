d23648af13320096bb5f58d37fc10037be74de70
soulomoon <fwy996602672@gmail.com>
2022-02-23 21:54:35 +0000

    update

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           system = "x86_64-linux";
           modules = 
           [ 
-            ./nixosDesk/configuration.nix 
+            ./nixos/configuration.nix 
           ];
         };
 
