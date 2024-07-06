f66b883eab0f1443790cc20319711402f0f686ea
Patrick <fwy996602672@gmail.com>
2024-07-06 22:32:58 +0800

    update home

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -2,17 +2,37 @@
 
 let ConfigFile = "~/.config/nixpkgs";
 in {
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
+  };
   programs.zsh = {
     enable = true;
-    initExtra = '' 
+    initExtra = ''
       export TERM="xterm-256color"
+      export PATH=~/bin:$PATH
+      export PATH="/opt/homebrew/opt/m4/bin:$PATH"
       export ConfigFile="${ConfigFile}"
-      export C_INCLUDE_PATH="`xcrun --show-sdk-path`/usr/include/ffi"
-      source ~/.p10k.zsh 
-      CASE_SENSITIVE="false"
-      eval $(thefuck --alias)
+      CASE_SENSITIVE="true"
+      # export C_INCLUDE_PATH="`xcrun --show-sdk-path`/usr/include/ffi"
+      # source ~/.p10k.zsh
+      # eval $(thefuck --alias)
       # [[ ! -r ~/.opam/opam-init/init.zsh ]] || source ~/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
 
+      # export PATH=~/.emacs.d/bin:$PATH
+      # export PATH=~/.cargo/bin:$PATH
+      # export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH"
+      # for ghc compilation
     '';
     shellAliases = {
       mc = "code ${ConfigFile}";
@@ -28,8 +48,11 @@ in {
 
       t = "tmux attach -t default || tmux new -s default";
     };
+
+
+
     oh-my-zsh = {
-      enable = true;
+      enable = false;
       plugins = [ "brew" "fasd"];
       extraConfig = ''
         export PATH=~/.emacs.d/bin:$PATH
@@ -38,6 +61,7 @@ in {
         # export PATH=/opt/homebrew/Cellar/pcre/8.45/include/:$PATH
         export NVM_DIR="$HOME/.nvm"
         export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH"
+        export PATH="/opt/homebrew/opt/m4/bin:$PATH"
         [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
         [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
       '';
@@ -49,7 +73,7 @@ in {
         { name = "zsh-users/zsh-syntax-highlighting"; }
         { name = "zsh-users/zsh-completions"; }
         { name = "chisui/zsh-nix-shell"; }
-        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+        # { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
       ];
     };
 
